a9cf7a82b191e3a5e74ca7586ec22243b350ba83
Felipe Santos <felipecassiors@gmail.com>
2023-04-24 23:28:50 -0300

    build(deps): update `oclif` to latest version and several other dependencies (#183)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-import { Command, flags } from "@oclif/command";
+import { Command, Flags } from "@oclif/core";
 import { cosmiconfig } from "cosmiconfig";
 import * as download from "download";
 import * as Listr from "listr";
+// eslint-disable-next-line unicorn/import-style
 import * as chalk from "chalk";
 import * as shell from "shelljs";
 require("pkginfo")(module, "description");
 
-class Bindl extends Command {
+export class Bindl extends Command {
   static description = `${module.exports.description}
   The config will be read from any valid config file in the current directory. The configuration file can be defined using all the extensions and names accepted by ${chalk.blue(
     "cosmiconfig"
@@ -14,16 +15,16 @@ class Bindl extends Command {
   `;
 
   static flags = {
-    version: flags.version(),
-    help: flags.help({ char: "h" }),
-    config: flags.string({
+    version: Flags.version(),
+    help: Flags.help(),
+    config: Flags.string({
       char: "c",
       description: "Path to the config file",
     }),
   };
 
   async run(): Promise<void> {
-    const { flags } = this.parse(Bindl);
+    const { flags } = await this.parse(Bindl);
 
     // Return early if BINDL_SKIP is set
     if (process.env.BINDL_SKIP) {
@@ -51,72 +52,71 @@ class Bindl extends Command {
     ];
 
     // Load the custom decompressPlugins
-    if (result.config.decompressPlugins && result.config.decompressPlugins.length > 0) {
+    if (
+      result.config.decompressPlugins &&
+      result.config.decompressPlugins.length > 0
+    ) {
       for (const plugin of result.config.decompressPlugins) {
         plugins.push(require(plugin)());
       }
     }
 
-    result.config.binaries.forEach(
-      async (binary: {
-        platform: "linux" | "darwin" | "win32";
-        arch: "x64" | "x86";
-        url: string;
-        files: { source: string; target: string }[];
-      }) => {
-        tasks.add({
-          title: `downloading and extracting ${chalk.blue.underline(
-            binary.url
-          )}`,
-          skip: () => {
-            // If npm_config_arch is set, we only download the binary for the
-            // current platform and the arch set by npm_config_arch.
-            if (process.env.npm_config_arch) {
-              if (process.env.npm_config_arch !== binary.arch) {
-                return "npm_config_arch is set to a different arch";
-              }
-              // Check if current platform is the same as the binary platform
-              if (process.platform !== binary.platform) {
-                return "npm_config_arch is set and current platform is different from the binary platform";
-              }
+    for (const binary of result.config.binaries as {
+      platform: "linux" | "darwin" | "win32";
+      arch: "x64" | "x86";
+      url: string;
+      files: { source: string; target: string }[];
+    }[]) {
+      tasks.add({
+        title: `downloading and extracting ${chalk.blue.underline(binary.url)}`,
+        skip: () => {
+          // If npm_config_arch is set, we only download the binary for the
+          // current platform and the arch set by npm_config_arch.
+          if (process.env.npm_config_arch) {
+            if (process.env.npm_config_arch !== binary.arch) {
+              return "npm_config_arch is set to a different arch";
             }
-            return false
-          },
-          task: async () =>
-            download(
-              binary.url,
-              `./binaries/${binary.platform}/${binary.arch}`,
-              {
-                extract: true,
-                filter: (file) => {
-                  if (binary.files) {
-                    return Boolean(binary.files.find((f) => f.source === file.path))
-                  }
-                  return true
-                },
-                map: (file) => {
-                  if (binary.files) {
-                    const f = binary.files.find((f) => f.source === file.path);
-                    if (f) {
-                      file.path = f.target;
-                    }
-                  }
-                  return file;
-                },
-                plugins,
+
+            // Check if current platform is the same as the binary platform
+            if (process.platform !== binary.platform) {
+              return "npm_config_arch is set and current platform is different from the binary platform";
+            }
+          }
+
+          return false;
+        },
+        task: async () =>
+          download(binary.url, `./binaries/${binary.platform}/${binary.arch}`, {
+            extract: true,
+            filter: (file) => {
+              if (binary.files) {
+                return Boolean(
+                  binary.files.some((f) => f.source === file.path)
+                );
               }
-            ),
-        });
-      }
-    );
+
+              return true;
+            },
+            map: (file) => {
+              if (binary.files) {
+                const f = binary.files.find((f) => f.source === file.path);
+                if (f) {
+                  file.path = f.target;
+                }
+              }
+
+              return file;
+            },
+            plugins,
+          }),
+      });
+    }
 
     try {
-      await shell.rm("-rf", "./binaries");
+      shell.rm("-rf", "./binaries");
       await tasks.run();
-    } catch (error) {
-      await shell.exit(1);
+    } catch {
+      shell.exit(1);
     }
   }
 }
-
-export = Bindl;
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,13 +1,12 @@
-import { getBinPathSync } from "get-bin-path";
 import * as execa from "execa";
 import * as shell from "shelljs";
 import * as tmp from "tmp";
 
-const bin = getBinPathSync();
+const bin = `${process.cwd()}/bin/dev`;
 
 const configPath = `${process.cwd()}/test/res/bindl.config.js`;
 
-jest.setTimeout(30000);
+jest.setTimeout(30_000);
 
 beforeAll(() => {
   process.env = Object.assign(process.env, { FORCE_COLOR: 0 });
@@ -52,15 +51,33 @@ describe("bindl", () => {
     expect(result.stdout).toContain(`downloading and extracting`);
     expect(result.exitCode).toBe(0);
 
-    if (process.platform === "linux") {
-      expect(shell.test("-f", "./binaries/linux/x64/shellcheck")).toBeTruthy();
-    } else if (process.platform === "darwin") {
-      expect(shell.test("-f", "./binaries/darwin/x64/shellcheck")).toBeTruthy();
-    } else if (process.platform === "win32") {
-      expect(
-        shell.test("-f", "./binaries/win32/x64/shellcheck.exe")
-      ).toBeTruthy();
+    switch (process.platform) {
+      case "linux": {
+        expect(
+          shell.test("-f", "./binaries/linux/x64/shellcheck")
+        ).toBeTruthy();
+
+        break;
+      }
+
+      case "darwin": {
+        expect(
+          shell.test("-f", "./binaries/darwin/x64/shellcheck")
+        ).toBeTruthy();
+
+        break;
+      }
+
+      case "win32": {
+        expect(
+          shell.test("-f", "./binaries/win32/x64/shellcheck.exe")
+        ).toBeTruthy();
+
+        break;
+      }
+      // No default
     }
+
     expect(shell.test("-f", "./binaries/linux/arm/shellcheck")).toBeFalsy();
     expect(shell.test("-f", "./binaries/linux/arm64/shellcheck")).toBeFalsy();
     expect(shell.test("-f", "./binaries/win32/x64/shellcheck.exe")).toBeFalsy();
